157628792c38173654fce943cb75a2966369866e
Jonathan Herrera <msjona28@outlook.com>
2023-10-17 00:52:14 -0700

    Commit inicial

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+process.env.port = 3001;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Backend del sistema de donaciones</h1>");
+})
+
+app.listen(process.env.port, () => {
+    console.log(`Escuchando el puerto ${process.env.port}`);
+}).on('error', err => {
+    console.log('Error al iniciar el servidor: ', err);
+})
